7e14ff6607d2ac7331e3df759a9c10624d954747
Jerodsun <Jerod.sun@live.com>
2019-07-28 13:53:58 -0700

    Updates

---
+++ precache-manifest.2e6a55cc91322c2a8ac277b797a7c0ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aad76ef98e6a6c9e941e0cb414297267",
+    "url": "/React-Frontend-Practice/index.html"
+  },
+  {
+    "revision": "630504b30afcd675f35f",
+    "url": "/React-Frontend-Practice/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "cf9dd6672d373b336bb1",
+    "url": "/React-Frontend-Practice/static/css/main.5848931e.chunk.css"
+  },
+  {
+    "revision": "630504b30afcd675f35f",
+    "url": "/React-Frontend-Practice/static/js/2.77527976.chunk.js"
+  },
+  {
+    "revision": "cf9dd6672d373b336bb1",
+    "url": "/React-Frontend-Practice/static/js/main.3aca8e0e.chunk.js"
+  },
+  {
+    "revision": "f859d6ca545654aa1752",
+    "url": "/React-Frontend-Practice/static/js/runtime~main.8c5c09f8.js"
+  }
+]);
