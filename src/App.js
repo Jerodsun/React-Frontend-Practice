12a497ecdb27687eea33107f037de96a5a2178ed
Jerodsun <Jerod.sun@live.com>
2019-07-27 17:14:27 -0700

    alert click

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={function() {
+        alert('click');}}>
         {this.props.value}
       </button>
     );
