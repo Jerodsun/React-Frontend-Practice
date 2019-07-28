725b35699091cafb3c04981e12313e5c92dadf89
Jerodsun <Jerod.sun@live.com>
2019-07-28 11:18:24 -0700

    scss refactoring

--- src/newApp.js
+++ src/newApp.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import '../node_modules/react-vis/dist/style.css';
 import {XYPlot, LineSeries} from 'react-vis';
 
